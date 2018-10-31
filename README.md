1f6b47dd8d3f6c3b2cbb58122b77dd5319133ed7
Dídac García <didaquis@gmail.com>
2018-10-31 21:16:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Phaser 3 introduction Example
+
+A Phaser 3 introduction example using Webpack template (https://github.com/photonstorm/phaser3-project-template.git)
+
+### Requirements
+
+We need [Node.js](https://nodejs.org) to install and run scripts.
+
+## Install and run
+
+Run next commands in your terminal:
+
+| Command | Description |
+|---------|-------------|
+| `npm install` | Install dependencies |
+| `npm start` | Launch browser to show the examples. <br> Press `Ctrl + c` to kill **http-server** process. |
+=======
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	entry: {
+		index: './src/index.js'
+	},
+
+	module: {
+		rules: [
+			{
+				test: [ /\.vert$/, /\.frag$/ ],
+				use: 'raw-loader'
+			},
+
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src/'),
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			}
+		]
+	},
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					chunks: 'all',
+					priority: 1
+				}
+			}
+		}
+	},
+
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/build/',
+		filename: '[name].js'
+	},
+
+	performance: {
+		hints: false
+	},
+
+	plugins: [
+		new webpack.DefinePlugin({
+			'CANVAS_RENDERER': JSON.stringify(true),
+			'WEBGL_RENDERER': JSON.stringify(true)
+		})
+	],
+};
