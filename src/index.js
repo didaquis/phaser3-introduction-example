de4b88ff1c7e95af33a9f2142ed3b7ed57391be6
Dídac García <didaquis@gmail.com>
2018-12-26 19:35:49 +0100

    Added some comments

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ var scoreText;
 var game = new Phaser.Game(config); // eslint-disable-line no-undef
 
 function preload() {
+	// Load assets
 	this.load.image('sky', 'assets/sky.png');
 	this.load.image('ground', 'assets/platform.png');
 	this.load.image('star', 'assets/star.png');
@@ -39,19 +40,22 @@ function preload() {
 
 function create() {
 	//  A simple background for our game
-	this.add.image(400, 300, 'sky');
+	this.add.image(400, 300, 'sky'); // Observe 3th argument, it's an asset preload before
 
-	//  The platforms group contains the ground and the 2 ledges we can jump on
+	//  The platforms group contains the ground and the 2 ledges we can jump on. We set group of the platforms as static items, this mean platformas don't have gravity
 	platforms = this.physics.add.staticGroup();
 
 	//  Here we create the ground.
 	//  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody(); // refresBody is needed if you are using setScale.
 
 	//  Now let's create some ledges
 	platforms.create(600, 400, 'ground');
 	platforms.create(50, 250, 'ground');
 	platforms.create(750, 220, 'ground');
+
+
+
 
 	// The player and its settings
 	player = this.physics.add.sprite(100, 450, 'dude');
@@ -60,20 +64,18 @@ function create() {
 	player.setBounce(0.2);
 	player.setCollideWorldBounds(true);
 
-	//  Our player animations, turning, walking left and walking right.
+	//  Our player animations, turning, walking left and walking right. With this methos, qe are configuring the visualization of player sprite whena player is in movement.
 	this.anims.create({
 		key: 'left',
 		frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
 		frameRate: 10,
 		repeat: -1
 	});
-
 	this.anims.create({
 		key: 'turn',
 		frames: [ { key: 'dude', frame: 4 } ],
 		frameRate: 20
 	});
-
 	this.anims.create({
 		key: 'right',
 		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
@@ -84,6 +86,8 @@ function create() {
 	//  Input Events
 	cursors = this.input.keyboard.createCursorKeys();
 
+
+
 	//  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
 	stars = this.physics.add.group({
 		key: 'star',
@@ -92,29 +96,31 @@ function create() {
 	});
 
 	stars.children.iterate(function(child) {
-
 		//  Give each star a slightly different bounce
 		child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)); // eslint-disable-line no-undef
-
 	});
 
-	bombs = this.physics.add.group();
+
+
+	bombs = this.physics.add.group(); // create a new group and apply the physics
+
+
 
 	//  The score
 	scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-	//  Collide the player and the stars with the platforms
-	this.physics.add.collider(player, platforms);
+	//  Collide the player and the stars with the platforms. Define collision between objects
+	this.physics.add.collider(player, platforms); // now the player can walk over platforms
 	this.physics.add.collider(stars, platforms);
 	this.physics.add.collider(bombs, platforms);
 
 	//  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
 	this.physics.add.overlap(player, stars, collectStar, null, this);
 
-	this.physics.add.collider(player, bombs, hitBomb, null, this);
+	this.physics.add.collider(player, bombs, hitBomb, null, this); // If player hit a bomb, "hitBomb" function is called as callback
 }
 
-function update() {
+function update() { // This is the update loop
 	if (gameOver) {
 		return;
 	}
@@ -133,13 +139,13 @@ function update() {
 		player.anims.play('turn');
 	}
 
-	if (cursors.up.isDown && player.body.touching.down) {
-		player.setVelocityY(-330);
+	if ( (cursors.up.isDown && player.body.touching.down) || (cursors.space.isDown && player.body.touching.down) ) {
+		player.setVelocityY(-340);
 	}
 }
 
 function collectStar(player, star) {
-	star.disableBody(true, true);
+	star.disableBody(true, true); // Game Object is made inactive and invisible, which removes it from display
 
 	//  Add and update the score
 	score += 10;
@@ -148,9 +154,7 @@ function collectStar(player, star) {
 	if (stars.countActive(true) === 0) {
 		//  A new batch of stars to collect
 		stars.children.iterate(function(child) {
-
 			child.enableBody(true, child.x, 0, true, true);
-
 		});
 
 		var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400); // eslint-disable-line no-undef
@@ -171,5 +175,5 @@ function hitBomb(player, bomb) {
 
 	player.anims.play('turn');
 
-	gameOver = true;
+	gameOver = true; // avoid execute logig of updtae loop
 }
